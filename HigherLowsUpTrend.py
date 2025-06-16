4eedb2194066bb1f9386c698e445f778c566d0ac
TateAlexander <tatesmith02@gmail.com>
2025-06-16 10:48:06 -0500

    Update HigherLowsUpTrend.py

--- HigherLowsUpTrend.py
+++ HigherLowsUpTrend.py
@@ -31,3 +31,59 @@ print(neighboringLows(security))
 
 plt.plot(security["Low"])
 plt.show()
+
+#-----------------------------
+#    Updated version below:
+
+"""Capture a stocks up trend by connecting the lows that are lower than its two neighbors"""
+import numpy as np
+import matplotlib.pyplot as plt
+import yfinance as fy
+
+security = fy.ticker.Ticker("TQQQ").history(interval='1d', start='2025-04-01', end="2025-06-13")
+
+def neighboringLows(stock_df):
+    nls = []
+    for i, low in enumerate(stock_df["Low"]):
+        if i > 0 and i < len(stock_df["Low"])-1 :
+            if low < stock_df["Low"][i-1] and low < stock_df["Low"][i+1]:
+                nls.append((i, low))
+
+    trends = []
+    trend = []
+    closeUpTrends = []
+    for nls_i, (low_i, low) in enumerate(nls):
+        if nls_i < len(nls)-1:
+            if low < nls[nls_i+1][1]:
+                if len(trend) == 0:
+                    trend.append(nls[nls_i])
+
+                trend.append(nls[nls_i+1])
+            else:
+                if len(trend) > 0:
+                    trends.append(trend)
+                    closeUpTrends.append([x[0] for x in trend])
+                    trend = []
+
+    return closeUpTrends
+
+def makeTrendLine(cuts, stock_df):
+    t = []
+    for trend in cuts:
+
+        uptrend = stock_df["Close"][trend[0]: trend[-1]]
+        uptrendStd = uptrend.std()
+        x = [num for num in np.arange(trend[0], trend[-1], 1)]
+        reg = np.polyfit(x, uptrend, deg=1)
+        trend = np.polyval(reg, x)
+
+        plt.plot(x, trend, 'r')
+        plt.plot(x, trend + uptrendStd, 'g--')
+        plt.plot(x, trend - uptrendStd, 'g--')
+        plt.plot(x, uptrend)
+        plt.show()
+
+nl = neighboringLows(security)
+
+print(nl)
+print(makeTrendLine(nl, security))
